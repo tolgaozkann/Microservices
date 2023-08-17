65b0b38a36cff72708bd3f2c4b96f9aec4625953
tolgaozkann <ozkanozkan3233@gmail.com>
2023-08-17 16:02:28 +0300

    dtos added

---
+++ Services/Basket/Microservices.Services.Basket.WebAPI/Dtos/BasketDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Microservices.Services.Basket.WebAPI.Dtos;
+
+public class BasketDto
+{
+   public string UserId { get; set; } 
+   public string DiscountCode { get; set;}
+   public ICollection<BasketItemDto> BasketItems { get; set; }
+   public decimal TotalPrice
+   {
+       get => BasketItems.Sum(b => b.Price * b.Quantity);
+   }
+}
---
+++ Services/Basket/Microservices.Services.Basket.WebAPI/Dtos/BasketItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Microservices.Services.Basket.WebAPI.Dtos;
+
+public class BasketItemDto
+{
+    public int Quantity { get; set; }
+    public string CourseId { get; set; }
+    public string CourseName { get; set; }
+    public decimal Price { get; set; }
+}
